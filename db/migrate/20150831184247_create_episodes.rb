67fb28d1846395105ec6e0f2c7d7130176ff0735
sayginburak <sayginburak17@gmail.com>
2015-08-31 21:43:10 +0300

    Episode model created.

---
+++ db/migrate/20150831184247_create_episodes.rb
@@ -0,0 +1,15 @@
+class CreateEpisodes < ActiveRecord::Migration
+  def change
+    create_table :episodes do |t|
+      t.string :name
+      t.string :image_link
+      t.string :airdate
+      t.string :imdb_link
+      t.string :imdb_point
+      t.integer :season_no
+      t.belogs_to :show
+
+      t.timestamps null: false
+    end
+  end
+end
